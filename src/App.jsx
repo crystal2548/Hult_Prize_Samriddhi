4253183e68b40db10f45e0698e10a6858ed882a0
Leejaw-Chitrakar <leejaw.chitrakar10@gmail.com>
2026-02-09 13:58:31 +0545

    OC added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // import React, {useEffect, useState} from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
-// import Cards1 from './Cards1'
 import Test from './Test'
 import './App.css'
 const App = () => {
@@ -10,9 +9,6 @@ const App = () => {
    
     <RouterProvider router={router} />
     // <Test />
-    // <div>
-       // {/* <Cards1 /> */}
-    // </div>
   )
 }
 
--- src/data/organizersData.js
+++ src/data/organizersData.js
@@ -1,73 +1,89 @@
+import AadarsanDahal from "../assets/OC/AadarsanDahal.png";
+import SagunShrestha from "../assets/OC/SagunShrestha.png";
+import DikshantNeupane from "../assets/OC/DikshantNeupane.png";
+import BinitaGautam from "../assets/OC/BinitaGautam.png";
+import AchyutaGajurel from "../assets/OC/AchyutaGajurel.png";
+import AayushPandey from "../assets/OC/AayushPandey.png";
+import KushalSuwal from "../assets/OC/KushalSuwal.png";
+import AayushmaLuitel from "../assets/OC/AayushmaLuitel.png";
+import NimeshAdhikari from "../assets/OC/NimeshAdhikari.png";
+import SumnimaKarki from "../assets/OC/SumnimaKarki.png";
+import KarunAcharya from "../assets/OC/KarunAcharya.png";
+import PritikaThapa from "../assets/OC/PritikaThapa.png";
+import SwastikRawat from "../assets/OC/SwastikRawat.png";
+import SarishmaGhimire from "../assets/OC/SarishmaGhimire.png";
+
+
 const organizersData = [
     { 
         name: "Aadarsan Dahal", 
         role: "Campus Director",
-        image: ""
+        image: AadarsanDahal
     },
     { 
         name: "Sagun Shrestha", 
         role: "Deputy CD",
-        image: ""
+        image: SagunShrestha
     },
     { 
         name: "Dikshant Neupane", 
         role: "Marketing and Communication",
-        image: ""
+        image: DikshantNeupane
     },
     { 
         name: "Binita Gautam", 
         role: "Event Management",
-        image: ""
+        image: BinitaGautam
     },
     { 
-        name: "Achyuta Gajurel", 
+        name: "Achyuta Gajurel",
         role: "Event Management",
-        image: ""
+        image: AchyutaGajurel
     },
     { 
         name: "Aayush Pandey", 
         role: "Event Management",
-        image: ""
+        image: AayushPandey
     },
     { 
         name: "Kushal Suwal", 
         role: "Graphics Designer",
-        image: ""
+        image: KushalSuwal
     },
     { 
         name: "Aayushma Luitel", 
         role: "Social media",
-        image: ""
+        image: AayushmaLuitel
     },
     { 
         name: "Nimesh adhikari", 
         role: "Video Editor",
-        image: ""
+        image: NimeshAdhikari
     },
     { 
         name: "Sumnima Karki", 
         role: "Social media",
-        image: ""
+        image: SumnimaKarki
     },
     { 
         name: "Karun Acharya", 
         role: "Techinical Head",
-        image: ""
+        image: KarunAcharya
     },
     { 
         name: "Pritika Thapa", 
         role: "Logistic",
-        image: ""
+        image: PritikaThapa
     },
     { 
         name: "Swastik Rawat", 
         role: "Logistic",
-        image: ""
+        image: SwastikRawat
     },
     { 
         name: "Sarishma Ghimire", 
         role: "Startup team Advisor",
-        image: ""
+        image: SarishmaGhimire
     }
 ];
 
