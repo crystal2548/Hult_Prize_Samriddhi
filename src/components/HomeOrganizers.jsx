55753a6890c645e1f5edbf2e000dd158d7ba3986
Samit <shresthasamit40@gmail.com>
2026-02-10 14:00:40 +0545

    feat:backend work

--- src/components/HomeOrganizers.jsx
+++ src/components/HomeOrganizers.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/homeOrganizers.css';
-import organizersData from '../data/organizersData.js';
+// import organizersData from '../data/organizersData.js';
 import { LinkedinFilled, TwitterOutlined } from '@ant-design/icons';
 
+import { getAllOrganizers } from '../lib/services/organizations.service.js';
+
 const HomeOrganizers = () => {
+    const [organizers, setOrganizers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        async function getOrganizers() {
+            try {
+                const data = await getAllOrganizers();
+                if (!data) {
+                    return
+                }
+                setOrganizers(data);
+                // console.log(organizers);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+
+        getOrganizers();
+    }, [])
     return (
         <section className="home-organizers-section">
             <div className="home-organizers-container">
@@ -15,12 +36,12 @@ const HomeOrganizers = () => {
                 </div>
 
                 <div className="home-organizers-grid">
-                    {organizersData.map((member, index) => (
+                    {organizers.map((member, index) => (
                         <div key={index} className="home-organizer-card">
                             <div className="organizer-img-wrapper">
-                                <img 
-                                    src={member.image} 
-                                    alt={member.name} 
+                                <img
+                                    src={member.image}
+                                    alt={member.name}
                                     className="organizer-img"
                                 />
                                 <div className="organizer-social-overlay">
